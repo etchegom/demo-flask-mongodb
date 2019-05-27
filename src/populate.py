91c74234159eefbd9c7a750e38df57685e263b72
Matthieu Etchegoyen <etchegom@gmail.com>
2019-05-27 22:07:33 +0200

    fix tox issues

--- src/populate.py
+++ src/populate.py
@@ -4,9 +4,15 @@ if __name__ == "__main__":
     from omdb_fixtures_loader import loader
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--apikey", dest="api_key", required=True, help="OMDB API key")
-    parser.add_argument("--search", dest="search", required=True, help="The text to search")
-    parser.add_argument("--type", dest="media_type", required=False, help="Type of media")
+    parser.add_argument(
+        "--apikey", dest="api_key", required=True, help="Your OMDB API key"
+    )
+    parser.add_argument(
+        "--search", dest="search", required=True, help="The text to search"
+    )
+    parser.add_argument(
+        "--type", dest="media_type", required=False, help="Type of media"
+    )
     args = parser.parse_args()
 
     for hit in loader.search_and_fetch(
