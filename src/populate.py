96251217e26655287c5ea702c6c1eae97449722e
Matthieu Etchegoyen <etchegom@gmail.com>
2019-05-27 21:48:20 +0200

    use omdb-fixtures-loader component in populate script

--- src/populate.py
+++ src/populate.py
@@ -1,11 +1,15 @@
 if __name__ == "__main__":
     import argparse
+    from pprint import pprint
+    from omdb_fixtures_loader import loader
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("apikey", help="OMDB API key", type=str)
+    parser.add_argument("--apikey", dest="api_key", required=True, help="OMDB API key")
+    parser.add_argument("--search", dest="search", required=True, help="The text to search")
+    parser.add_argument("--type", dest="media_type", required=False, help="Type of media")
     args = parser.parse_args()
 
-    print(args)
-
-# http --json "http://www.omdbapi.com/?apikey=eb88cc&r=json&type=movie&s=star+wars"
-# http --json "http://www.omdbapi.com/?apikey=eb88cc&r=json&type=movie&s=avengers"
-# http --json "http://www.omdbapi.com/?apikey=eb88cc&r=json&i=tt2395427"
+    for hit in loader.search_and_fetch(
+        api_key=args.api_key, search=args.search, media_type=args.media_type
+    ):
+        pprint(hit)
