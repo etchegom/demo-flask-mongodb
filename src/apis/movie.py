44c44bba3ac3757016487a6a93d14b98d56dbbe8
Matthieu Etchegoyen <etchegom@gmail.com>
2019-10-08 10:06:12 +0200

    Some code improvements (#2)
    * use flask CLI, apply mypy better and extract isort config to a dedicated file
    
    * update README and add logs in populate script

--- src/apis/movie.py
+++ src/apis/movie.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from flask_restplus import Namespace, Resource, fields
 
 from models import MovieDocument
@@ -11,5 +13,5 @@ movie_model = api.model("Movie", model={"title": fields.String})
 @api.route("/")
 class MovieList(Resource):
     @api.marshal_with(movie_model, as_list=True, envelope="data")
-    def get(self):
+    def get(self) -> List[object]:
         return MovieDocument.objects.only("title").paginate(page=1, per_page=2).items
